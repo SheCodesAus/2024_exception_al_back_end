b701a1b39a9c3ff11222d16d6ae069ff08e552f4
Denise Siddons <146698245+SiddonsD@users.noreply.github.com>
2024-03-21 13:46:22 +0800

    Update wsgi.py

--- exception_al/exception_al/wsgi.py
+++ exception_al/exception_al/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for exception_al_back_end project.
+WSGI config for exception_al project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exception_al_back_end.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exception_al.settings')
 
 application = get_wsgi_application()
