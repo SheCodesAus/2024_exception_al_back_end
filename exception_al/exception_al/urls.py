9d3e281af173bada53077536e38854ebdf1e8e00
Denise Siddons <146698245+SiddonsD@users.noreply.github.com>
2024-03-21 13:45:55 +0800

    Update urls.py

--- exception_al/exception_al/urls.py
+++ exception_al/exception_al/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for exception_al_back_end project.
+URL configuration for exception_al project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
