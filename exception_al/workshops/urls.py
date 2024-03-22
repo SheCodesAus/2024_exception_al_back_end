3fe59a954a44f440ea29ea410413b72b73c67c60
mkeerthiasm <mkeerthi.ayala@gmail.com>
2024-03-22 22:13:07 +0800

    created api code for returning list of workshops

--- exception_al/workshops/urls.py
+++ exception_al/workshops/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include # This imports the path and include functions from the django.urls module
-from .views import WorkshopCreateView # This imports the WorkshopCreateView class from the views.py file
+from .views import WorkshopCreateView , WorkshopListView  # This imports the WorkshopCreateView and WorkshopListView views from the workshops.views module
 
 
 urlpatterns = [
     path('workshops/', WorkshopCreateView.as_view(), name='workshop-create'), # This maps the URL /workshops/ to the WorkshopCreateView view
-    
+    path('workshops/', WorkshopListView.as_view(), name='workshop-list'), # This maps the URL /workshops/ to the WorkshopListView view
+
 ]
--- exception_al/workshops/views.py
+++ exception_al/workshops/views.py
@@ -6,6 +6,7 @@ from .serializers import WorkshopSerializer
 from django.http import Http404
 from rest_framework import status
 
+
 # Create your views here.
 # This is the view for creating a new workshop
 
@@ -17,4 +18,37 @@ class WorkshopCreateView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+# This is the view for getting a list of all workshops
+    
+class WorkshopListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        workshops = Workshop.objects.all()
+        serializer = WorkshopSerializer(workshops, many=True)
+        return Response(serializer.data)
+
+class WorkshopDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, request, workshop_id):
+        try:
+            return Workshop.objects.get(pk=workshop_id)
+        except Workshop.DoesNotExist:
+            raise Http404("Workshop does not exist")
+
+    def get(self, request, pk, format=None):
+        workshop = self.get_object(pk)
+        serializer = WorkshopSerializer(workshop)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format = None):
+        workshop = self.get_object(pk)
+        serializer = WorkshopSerializer(workshop, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
