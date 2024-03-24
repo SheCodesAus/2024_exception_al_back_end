9d295f28c157e866e9e7c78d672ea78139c44208
SiddonsD <denise.siddons@gmail.com>
2024-03-24 23:02:27 +0800

    removes duplicate api-token-auth url in users app

--- exception_al/user/urls.py
+++ exception_al/user/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('users/', views.CustomUserList.as_view()),
     path('user/register', views.CustomUserRegister.as_view()),
     path('user/<int:pk>/', views.CustomUserDetail.as_view()),
-    path('api-token-auth/', views.CustomAuthToken.as_view(), name='api_token_auth')
 ]
