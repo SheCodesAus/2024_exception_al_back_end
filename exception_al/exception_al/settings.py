2df4776c32776d32edf775134ea9e2cdda70f1c0
mkeerthiasm <mkeerthi.ayala@gmail.com>
2024-03-24 22:35:50 +0800

    updated settings.py, added permissions.py,added view.py by added permission class

--- exception_al/exception_al/settings.py
+++ exception_al/exception_al/settings.py
@@ -32,11 +32,10 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'user.apps.UserConfig',
+    'workshops.apps.WorkshopsConfig', # This is the WorkshopsConfig class from the apps.py file in the workshops app
     'rest_framework',
     'rest_framework.authtoken',
-    'django.contrib.admin',
-    'workshops.apps.WorkshopsConfig', # This is the WorkshopsConfig class from the apps.py file
-    'rest_framework', # This is the Django REST framework
+    'django.contrib.admin',   
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -45,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 AUTH_USER_MODEL = 'user.CustomUser'
+                  
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
