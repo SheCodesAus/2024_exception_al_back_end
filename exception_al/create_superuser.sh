c421479f88861f67670050f2833081bf3d15da16
Suan Choi <siasiarhdwn@gmail.com>
2024-04-01 18:09:09 +0800

    modify run.sh file

--- exception_al/create_superuser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@email.com', 'Password1!')" | python3 manage.py shell
--- exception_al/run.sh
+++ exception_al/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python manage.py migrate
-python manage.py createsuperuser --no-input
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@email.com', 'Password1!')" | python manage.py shell
 gunicorn --bind :8000 --workers 1 exception_al.wsgi
