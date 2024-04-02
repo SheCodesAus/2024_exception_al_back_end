4830750125b53f9ffecc3698f6dac363e93d4128
Suan Choi <siasiarhdwn@gmail.com>
2024-04-02 19:55:29 +0800

    add makemigrations step

--- exception_al/run.sh
+++ exception_al/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python manage.py flush --no-input
+python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --no-input
 gunicorn --bind :8000 --workers 1 exception_al.wsgi
