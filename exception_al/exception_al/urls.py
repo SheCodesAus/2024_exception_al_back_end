ace645ebc4dc8b4d57e511771ca5303929007460
Suan Choi <siasiarhdwn@gmail.com>
2024-04-03 20:24:12 +0800

    feat: add absolute path for media

--- exception_al/exception_al/urls.py
+++ exception_al/exception_al/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from user.views import CustomAuthToken
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -26,4 +28,7 @@ urlpatterns = [
     path('', include('workshops.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('api-token-auth/', CustomAuthToken.as_view(), name='api_token_auth')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
