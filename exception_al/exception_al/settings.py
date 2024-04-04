669c922af5247d77bf04d073463396647ca929c3
Suan Choi <siasiarhdwn@gmail.com>
2024-04-04 19:56:36 +0800

    turn debug on

--- exception_al/exception_al/settings.py
+++ exception_al/exception_al/settings.py
@@ -27,11 +27,11 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get(
-    'DJANGO_DEBUG',
-    'False'
-)    != 'False'
-# DEBUG = True
+# DEBUG = os.environ.get(
+#     'DJANGO_DEBUG',
+#     'False'
+# )    != 'False'
+DEBUG = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
